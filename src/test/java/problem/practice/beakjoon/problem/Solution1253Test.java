1696441d478ed3c6e0a13ae299a899c1b6753837
laipuni <2tgb02023@gmail.com>
2024-04-05 10:25:10 +0900

    test : Solution1253

---
+++ src/test/java/problem/practice/beakjoon/problem/Solution1253Test.java
@@ -0,0 +1,60 @@
+package problem.practice.beakjoon.problem;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution1253Test {
+
+    @DisplayName("정렬되지 않은 배열을 정렬된 배열로 바꿔준다")
+    @Test
+    void mergeSort(){
+        //given
+        int[] arr = {8,7,6,5,4,3,2,1};
+        //when
+        Solution1253.mergeSort(arr,0, arr.length - 1);
+        //then
+        assertThat(arr).isEqualTo(new int[]{1,2,3,4,5,6,7,8});
+    }
+
+    @DisplayName("1,2,3,4,5,6,7,8,9,10을 입력했을 때 좋은 수는 8개이다.")
+    @Test
+    void findGOODNumberPositive(){
+        //given
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        //when
+        int result = Solution1253.findGOODNumber(arr);
+        //then
+        assertThat(result).isEqualTo(8);
+
+    }
+
+
+    @DisplayName("-10,-9,-8,-7,-6,-5,-4,-,3,-2,-1 을 입력했을 때 좋은 수는 8개이다.")
+    @Test
+    void findGOODNumberNegative(){
+        //given
+        int[] arr = {-10,-9,-8,-7,-6,-5,-4,-3,-2,-1};
+        //when
+        int result = Solution1253.findGOODNumber(arr);
+        //then
+        assertThat(result).isEqualTo(8);
+
+    }
+
+    @DisplayName("-5,-4,-3,-2,-1,0,1,2,3,4,5 을 입력했을 때 좋은 수는 10개이다.")
+    @Test
+    void findGOODNumber(){
+        //given
+        int[] arr = {-5,-4,-3,-2,-1,0,1,2,3,4,5};
+        //when
+        int result = Solution1253.findGOODNumber(arr);
+        //then
+        assertThat(result).isEqualTo(11);
+    }
+}
