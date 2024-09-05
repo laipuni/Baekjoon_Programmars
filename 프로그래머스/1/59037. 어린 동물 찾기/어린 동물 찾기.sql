11c3c13617ff1beeb2529ec13b08952c9dbafcdb
laipuni <144443568+laipuni@users.noreply.github.com>
2024-09-05 09:36:58 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,2 +1,4 @@
 -- 코드를 입력하세요
-SELECT animal_id,name from animal_ins as a where a.intake_condition not like "aged"
+SELECT animal_id,name 
+from animal_ins as a 
+where a.intake_condition not like "aged"
