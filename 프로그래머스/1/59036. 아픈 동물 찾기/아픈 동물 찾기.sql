f3f42349e3b96f8eccc326f435616b6fa625943a
laipuni <144443568+laipuni@users.noreply.github.com>
2024-09-05 11:04:59 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,4 +1,3 @@
--- 코드를 입력하세요
-SELECT animal_id,name 
-from animal_ins as a 
-where a.INTAKE_CONDITION LIKE "Sick"
+select a.ANIMAL_ID, a.NAME
+from ANIMAL_INS as a
+where a.INTAKE_CONDITION like "Sick"
