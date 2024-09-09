959ba44fa15191ec7e59d0dea0ec58acfc773ee2
laipuni <144443568+laipuni@users.noreply.github.com>
2024-09-09 16:42:08 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -1,2 +1,3 @@
--- 코드를 입력하세요
-SELECT count(*) as USERS from user_info as u where u.age >= 20 and u.age <= 29 and year(joined) = 2021
+select count(*)
+from USER_INFO as u
+where year(u.joined) = 2021 and u.age >= 20 and u.age <= 29
